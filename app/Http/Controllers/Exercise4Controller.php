c7494042e420a49617ab85e869979cf7ed6735e4
kosuke-3136 <w.kosuke1005@icloud.com>
2026-06-24 16:50:42 +0900

    Suggest eager loading to fix N+1 issue
    Updated comment to suggest eager loading for performance improvement.

--- app/Http/Controllers/Exercise4Controller.php
+++ app/Http/Controllers/Exercise4Controller.php
@@ -10,7 +10,6 @@ class Exercise4Controller extends Controller
     public function index()
     {
         $posts = Post::all(); // 🐛 N+1問題
-        // 修正例: $posts = Post::with('user')->get();
 
         return view('exercises.exercise4', compact('posts'));
     }
