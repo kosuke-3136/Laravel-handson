9a69b11f457be9808e559ac7dd01ee9b6d28dede
kosuke-3136 <w.kosuke1005@icloud.com>
2026-06-24 16:51:04 +0900

    Update Exercise5Controller.php

--- app/Http/Controllers/Exercise5Controller.php
+++ app/Http/Controllers/Exercise5Controller.php
@@ -10,7 +10,6 @@ class Exercise5Controller extends Controller
     public function index()
     {
         $posts = Post::all(); // 🐛 3箇所でN+1
-        // 修正例: $posts = Post::with(['user', 'category', 'comments'])->get();
 
         return view('exercises.exercise5', compact('posts'));
     }
